590363bab7ba4962bb49762d8d346a6985e37cfa
Pashin <chpunkpas09@gmail.com>
2024-12-05 16:43:26 +0300

    kruzhochki ui

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from PyQt6.QtWidgets import QApplication
 
 class MainApp(QtWidgets.QMainWindow):
     def __init__(self):
-        self.circle = []
+        self.cir = []
         super(MainApp, self).__init__()
         uic.loadUi("UI.ui", self)
         self.setGeometry(100, 100, 800, 600)
@@ -15,17 +15,17 @@ class MainApp(QtWidgets.QMainWindow):
 
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
-        for circle in self.circle:
+        for circle in self.cir:
             painter.setBrush(QtGui.QColor(255, 255, 0))
             painter.drawEllipse(circle[0], circle[1], circle[2], circle[2])
 
     def draw_circles(self):
-        self.circle.clear()
+        self.cir.clear()
         for _ in range(10):
             diameter = random.randint(20, 100)
             x = random.randint(0, self.width() - diameter)
             y = random.randint(0, self.height() - diameter)
-            self.circle.append((x, y, diameter))
+            self.cir.append((x, y, diameter))
         self.update()
 
 
